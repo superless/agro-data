c33558641e7c20ac8e269ee55a76aa069d5590f7
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-19 13:35:56 -0300

    creando modelo

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export * from './index-model';
+export * from './input-model';
+export * from './metadata';
+export * from './model';
---
+++ src/input-model/TractorInput.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { InputBase } from './InputBase';
+
+export interface TractorInput extends InputBase {
+    brand: string;
+    code: string;
+}
