2f5a7caab942c80d6527707fd22939db878a30df
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-19 17:49:52 -0300

    Eliminando archivos generados anteriormente

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is a TypeGen auto-generated file.
- * Any changes made to this file can be lost when this file is regenerated.
- */
-
-export * from './index-model';
-export * from './input-model';
-export * from './metadata';
-export * from './model';
