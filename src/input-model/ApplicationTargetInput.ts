b9b0c4b801a6e51d5001225a2f85ba160b37cfc4
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-19 17:27:32 -0300

    creando modelo

---
+++ src/input-model/ApplicationTargetInput.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { InputBase } from './InputBase';
+
+export interface ApplicationTargetInput extends InputBase {
+    abbreviation: string;
+    name: string;
+}
---
+++ src/model/Comment.ts
@@ -0,0 +1,16 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { DocumentLocal } from './DocumentLocal';
+
+export interface Comment extends DocumentLocal {
+    id: string;
+    clientId: string;
+    commentary: string;
+    created: Date;
+    idUser: string;
+    entityIndex: number;
+    entityId: string;
+}
