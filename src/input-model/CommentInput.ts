2ebffe997c22d25127ea665ac7e1e4d371fc2d5b
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-01-19 17:49:53 -0300

    creando modelo

---
+++ src/input-model/CommentInput.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { InputBase } from '../index-model/InputBase';
+
+export interface CommentInput extends InputBase {
+    commentary: string;
+    idUser: string;
+    entityIndex: number;
+    entityId: string;
+}
---
+++ src/model/OrderFolder.ts
@@ -0,0 +1,16 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { DocumentLocal } from './DocumentLocal';
+
+export interface OrderFolder extends DocumentLocal {
+    id: string;
+    clientId: string;
+    idPhenologicalEvent: string;
+    idApplicationTarget: string;
+    idSpecie: string;
+    idIngredient: string;
+    idIngredientCategory: string;
+}
