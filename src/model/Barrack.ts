da7e358c232e1aedf9b4736520c18f8ecb78f372
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-02-01 18:03:34 -0300

    creando modelo

---
+++ src/model/Barrack.ts
@@ -0,0 +1,20 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { ItemLocation } from './ItemLocation';
+
+export interface Barrack extends ItemLocation {
+    id: string;
+    clientId: string;
+    name: string;
+    idPlotLand: string;
+    hectares: number;
+    plantingYear: number;
+    numberOfPlants: number;
+    idVariety: string;
+    idPollinator: string;
+    idRootstock: string;
+    seasonId: string;
+}
