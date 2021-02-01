d708ef3fcff7e129e7515cf1660080a486b89950
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2021-02-01 17:49:29 -0300

    creando modelo

---
+++ src/input-model/NotificationEventInput.ts
@@ -0,0 +1,18 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { InputBase } from '../index-model/InputBase';
+import { NotificationType }  from  './../extra/NotificationType';
+import { GeoItem } from '../model/GeoItem';
+
+export interface NotificationEventInput extends InputBase {
+    idBarrack: string;
+    idPhenologicalEvent: string;
+    notificationType: NotificationType;
+    description: string;
+    created: Date;
+    base64: string;
+    location: GeoItem;
+}
---
+++ src/model/PreOrder.ts
@@ -0,0 +1,17 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { DocumentLocal } from './DocumentLocal';
+import { PreOrderType }   from   './../extra/PreOrderType';
+
+export interface PreOrder extends DocumentLocal {
+    id: string;
+    clientId: string;
+    name: string;
+    idIngredient: string;
+    orderFolderId: string;
+    preOrderType: PreOrderType;
+    barracksId: string[];
+}
